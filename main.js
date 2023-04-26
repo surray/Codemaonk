1755e078e0cb0d8ca7ef2c4fd7b639a1d520c0e1
Surendar <surejuly@gmail.com>
2023-04-26 23:28:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector("#canvas");
+//code here
+
+function myFunction(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+   
+    var ctx = canvas.getContext("2d");
+    
+    ctx.beginPath();
+    ctx.arc(x, y, 40, 0, 2 * Math.PI,true);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.stroke();
+
+  }
+  
+  function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+  }
